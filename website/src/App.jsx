47e45e533834611376ddf38a498c82f4478920a6
Jed Villapando <jvillapa@uoguelph.ca>
2026-03-16 11:36:07 -0400

    working on hero

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,15 +1,12 @@
-import Navbar from "./components/Navbar.jsx";
+//import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
-import Projects from "./components/projects.jsx";
-import Contact from "./components/Contact.jsx";
+//import Projects from "./components/projects.jsx";
+//import Contact from "./components/Contact.jsx";
 
 export default function App() {
   return (
     <main>
-      <Navbar />
-      <Hero />
-      <Projects />
-      <Contact />
+      <Hero/>
     </main>
   );
 }
--- website/src/components/Hero.jsx
+++ website/src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import {ReactTyped} from "react-typed";
+import banner from "../assets/sky.jpg";
+import profile from "../assets/profile.png";
+
 
 
 export default function Hero(){
@@ -8,7 +11,7 @@ export default function Hero(){
             <div className="hero-container">
                 <img 
                     className="hero-img"
-                    //src={banner}
+                    src={banner}
                     alt="Sky Banner"
                 />
             </div>
@@ -28,7 +31,20 @@ export default function Hero(){
                             loop
                             />
                     </h2>
+                    <p>I am a Aspiring software developer with a strong foundation in computer engineering 
+                        and a drive to create impactful technology.
+                    </p>
+                    <list>
+                    <li>2nd year Computer Engineering Student at University of Guelph</li>
+                    <li>Previous Technical Systems Analyst at RBC T&O </li>
+                    <li>Incomming Software Developer RBC T&O Summer 2026</li>
+                    <li>Passionate in software development</li>
+                    <li>Past envolvments with design teams and Robotics competitions</li>
+                </list>
                     
+                </div>
+                <div className="hero-photo">
+                    <img src={profile} alt="Jed Renzo Portrait" className="profile-img"/>
                 </div>
 
             </div>
