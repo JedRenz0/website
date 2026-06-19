20e61ad7999ce7b4a40e60838a3636b6d77094aa
Jed Villapando <jvillapa@uoguelph.ca>
2026-06-19 15:12:49 -0400

    add Vercel Analytics

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import Background from "./components/Background";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -30,6 +31,7 @@ export default function App() {
       <Projects />
       <Contact />
       <Footer />
+      <Analytics />
     </>
   );
 }
